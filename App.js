ffc8505afe7105e51b00b8bf2dfddbccae306762
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-02-07 10:31:07 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar, View, Text } from "react-native";
+import { StatusBar, View, Text, Button, Alert } from "react-native";
+import { SafeAreaView } from "react-native";
 
 export default function App(){
+  const vai = () => {
+    Alert.alert("Foi", "Oi")
+  };
+
   return (
     <>
+    <SafeAreaView>
+
     <StatusBar barStyle="default" />
+    
     <View>
-    <Text>Olá React Native</Text>
+    <Text>Topo/Cabeçalho</Text>
     </View>
+
+    <View>
+    <Text>Conteúdo...</Text>
+    </View>
+
+    <View>
+    <Text>Footer</Text>
+    <Button title="Lá nele" onPress={vai} />
+    </View>
+
+    </SafeAreaView>
     </>
 
   )  
